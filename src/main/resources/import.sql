70b78d42a18bd4561be0f04403b75e8d54da9392
maho.miyata <maho.miyata@syncthought.com>
2021-09-21 19:34:08 +0900

    ディレクトリ変更

--- src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO food(id, name, expireDate) VALUES (1, '缶詰', '2021/09/30');
-INSERT INTO food(id, name, expireDate) VALUES (2, 'レトルトカレー', '2022/01/22');
-INSERT INTO food(id, name, expireDate) VALUES (3, 'はちみつ', '2022/12/01');
