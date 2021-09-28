1a2b7fa20ddc27c4465b92dad9b07b48f4f64bc9
maho.miyata <maho.miyata@syncthought.com>
2021-09-28 19:34:42 +0900

    Foodindexがindex.htmlに読み込まれるように変更

--- edm-front/src/App.vue
+++ edm-front/src/App.vue
@@ -1,15 +1,19 @@
 <template>
+  <div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <FoodIndex/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import FoodIndex from './components/FoodIndex.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FoodIndex
   }
 }
 </script>
---
+++ edm-front/src/components/FoodIndex.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <h1>Hello FoodIndex</h1>
+      {{ info }}
+      {{ data }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'FoodIndex',
+   data () {
+      return {
+      data: [
+          {
+              "id": 3,
+              "name": "はちみつ",
+              "expireDate": "2022/12/01"
+          },
+          {
+              "id": 2,
+              "name": "レトルトカレー",
+              "expireDate": "2022/01/22"
+          },
+          {
+              "id": 10,
+              "name": "レトルトカレー",
+              "expireDate": "2023/02/02"
+          }
+      ],
+      info: null
+      }
+  }
+  ,
+  mounted () {
+      axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
