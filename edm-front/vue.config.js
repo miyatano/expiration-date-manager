dce7f6bb34f3c478bca67f114142202682851622
maho.miyata <maho.miyata@syncthought.com>
2021-09-29 18:59:03 +0900

    CROSエラー回避

--- edm-front/vue.config.js
+++ edm-front/vue.config.js
@@ -2,5 +2,10 @@ module.exports = {
     devServer: {
         port: 8888,
         disableHostCheck: true,
-    },
+        proxy: {
+            "/food/": {
+              target: "http://localhost:8080",
+            }
+        }
+    }
 };
