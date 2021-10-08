891bc24131ef3754ae19d808efe541b3359e3a39
maho.miyata <maho.miyata@syncthought.com>
2021-10-08 17:16:45 +0900

    色々試し中

--- edm-front/src/components/FoodIndex.vue
+++ edm-front/src/components/FoodIndex.vue
@@ -4,7 +4,12 @@
     <ul  v-for="food in foods" :key="food.id">
       <li>{{ food.name }}</li>
       <li>{{ food.expireDate }}</li>
+      <button @click="consoleLogId(food.id)">See what happen in console.log</button>
     </ul>
+    <button @click="deleteFood">Delete id:1</button>
+
+    <button @click="increment">Up vote</button>
+    <p>{{ count }}</p>
     <!--ToDo: 一覧の並べ替え？ -->
   </div>
 </template>
@@ -16,8 +21,27 @@ export default {
   name: 'FoodIndex',
    data () {
       return {
-      foods: null,
+        count: 4,
+        foods: null,
       }
+  },
+  methods: {
+    increment () {
+      this.count++;
+    },
+    consoleLogId (id) {
+      console.log("This item is no." + id);
+    },
+    getAll () {
+      axios
+      .get('/food/')
+      .then(response => (this.foods = response.data))
+    },
+    deleteFood () {
+      axios
+      .delete('/food/1')
+      .then(() => { this.getAll(); })
+    }
   },
   mounted () {
       axios
