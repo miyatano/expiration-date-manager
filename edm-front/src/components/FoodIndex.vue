10ff93ef7b18e02de4f3d9698e84446ad466e71c
maho.miyata <maho.miyata@syncthought.com>
2021-10-14 23:18:17 +0900

    いらないもの整理

--- edm-front/src/components/FoodIndex.vue
+++ edm-front/src/components/FoodIndex.vue
@@ -4,23 +4,12 @@
     <ul  v-for="food in foods" :key="food.id">
       <li>{{ food.name }}</li>
       <li>{{ food.expireDate }}</li>
-      <button @click="deleteFood(food.id)">DELETE THIS ITEM!</button>
-      <button @click="consoleLogId(food.id)">See what happen in console.log</button>
     </ul>
 
     <input v-model="foodName" placeholder="Food name" />
     <input v-model="expireDate" placeholder="expire date" />
     <button @click="postFood(foodName, expireDate)">Post</button>
 
-    <h2>お試しのアイテム</h2>
-    <button @click="postSoup">スープ増えるよ :)</button>
-    <button @click="increment">Up vote</button>
-    <p>{{ count }}</p>
-
-
-    <input v-model="message" placeholder="edit me" />
-    <p>Message is: {{ message }}</p>
-    <button @click="consoleLogMessage(message)">See what happen in console.log</button>
     <!--ToDo: 一覧の並べ替え？ -->
   </div>
 </template>
@@ -40,28 +29,10 @@ export default {
       }
   },
   methods: {
-    increment () {
-      this.count++;
-    },
-    consoleLogId (id) {
-      console.log("This item is no." + id);
-    },
-    consoleLogMessage(message) {
-      console.log(`Here is message from input: ${message}`);
-    },
     getAll () {
       axios
       .get('/food/')
       .then(response => (this.foods = response.data))
-    },
-    postSoup () {
-      const item = {
-          "name":"スープ",
-          "expireDate": "2023/02/02"
-      };
-      axios
-      .post('/food/', item)
-      .then(() => { this.getAll(); })
     },
     postFood (foodName, expireDate) {
       const item = {
@@ -81,8 +52,6 @@ export default {
   mounted () {
       this.getAll();
       console.log(this);
-
-      // 追加？
       // 更新？
   }
 }
