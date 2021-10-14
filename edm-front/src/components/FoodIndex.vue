a974bfc6ac5663970bdcea7629f8b9e585444dc9
maho.miyata <maho.miyata@syncthought.com>
2021-10-15 01:25:33 +0900

    見た目整える

--- edm-front/src/components/FoodIndex.vue
+++ edm-front/src/components/FoodIndex.vue
@@ -7,25 +7,26 @@
       </el-header>
       <el-main>
         <el-row class="row-bg" justify="center">
-          <el-col :span="16">
-            <div class="grid-content bg-purple-light">
+          <el-col :span="12">
+            <div class="grid-content food-form">
+              <h2 :style="`font-size: var(--el-font-size-extra-large);`">その食べ物、いつが賞味期限ですか？</h2>
               <el-form ref="form" label-width="120px">
-                <el-form-item label="食品名">
+                <el-form-item label="食べ物の名前">
                   <el-input v-model="foodName"></el-input>
                 </el-form-item>
-                <el-form-item label="賞味期限の日付">
+                <el-form-item label="賞味期限">
                   <el-input v-model="expireDate"></el-input>
                 </el-form-item>
-                <el-button type="primary" @click="postFood">Primary</el-button>
+                <el-button type="primary" @click="postFood">追加</el-button>
               </el-form>
             </div>
-            <div class="grid-content bg-purple-light">
+            <div class="grid-content">
               <el-table
                 :data="foods"
                 style="width: 100%"
               >
-                <el-table-column prop="name" label="食品名" sortable width="180" />
-                <el-table-column prop="expireDate" label="賞味期限の日付" width="180" />
+                <el-table-column prop="name" label="食品名" width="180" />
+                <el-table-column prop="expireDate" label="賞味期限の日付" sortable width="180" />
                 <el-table-column prop="address" label="Status" :formatter="formatter" />
               </el-table>
             </div>
@@ -86,5 +87,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.food-form {
+  background: rgb(244, 244, 245);
+  padding: 30px;
+  border-radius: 5px;
+  margin-bottom: 30px;
+}
 </style>
